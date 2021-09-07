25e7f449f7c3effc4dbb473a782368d577423e84
wooone <yayeahyeahya22@gmail.com>
2021-09-07 19:33:28 +0800

    add icon,avatar

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,42 @@
 <template>
+<el-container style="height: 620px">
+  <el-aside width="200px">
+    <div class="aside_avatar">
+      <el-avatar :size="70">
+          <img alt="" src="./assets/avatar1.png"/>
+      </el-avatar>
+      <h4>Stone</h4>
+          <div class="icon_social">
+            <a class="fb" href="https://www.facebook.com/yu.stone.3/" style="margin-right:20px; color: inherit">
+              <font-awesome-icon :icon="['fab', 'facebook-f']" />
+            </a>
+            <a class="ig" href="https://www.instagram.com/stnyu/" style="color: inherit;">
+              <font-awesome-icon :icon="['fab', 'instagram']" />
+            </a>
+          </div>
+    </div>
+  </el-aside>
+
   <el-container>
-    <el-header>
-      <h1>header</h1>
-      <el-affix :offset="10">
-        <el-button type="primary">距离顶部 10px</el-button>
-      </el-affix>
+    <el-header style="text-align: right; font-size: 12px">
+      <span>stnyu</span>
     </el-header>
 
     <el-main>
-      <h1>Main</h1>
-      <img alt="Vue logo" src="./assets/logo.png" />
-      <br />
-      <el-button>我是 ElButton</el-button>
-      <el-button type="primary">主要按钮</el-button>
-      <el-button type="success">成功按钮</el-button>
-      <el-button type="info">信息按钮</el-button>
-      <el-button type="warning">警告按钮</el-button>
-      <el-button type="danger">危险按钮</el-button>
-      <el-divider></el-divider>
-      <el-tabs v-model="activeName" @tab-click="handleClick">
-        <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-        <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-        <el-tab-pane label="定時任務補償" name="fourth"
-          >定时任务補償</el-tab-pane
-        >
-      </el-tabs>
-      <el-divider></el-divider>
-
       <el-row :gutter="20">
-        <el-col :span="6"><div class="grid-content bg-purple"><el-image :src="src1"></el-image></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"><el-image :src="src1"></el-image></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"><el-image :src="src1"></el-image></div></el-col>
-        <el-col :span="6"><div class="grid-content bg-purple"><el-image :src="src1"></el-image></div></el-col>
+        <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+          <el-card shadow="hover">
+            <img src="./assets/3.png" class="image">
+            <div style="padding: 14px;">
+              <span>好吃的汉堡</span>
+              <div class="bottom">
+                <time class="time">{{ currentDate }}</time>
+                <el-button type="text" class="button">操作按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
       </el-row>
-
       <el-row :gutter="20">
         <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
           <el-card shadow="hover">
@@ -49,74 +51,39 @@
           </el-card>
         </el-col>
       </el-row>
-
     </el-main>
 
-    <el-footer>Footer</el-footer>
   </el-container>
+</el-container>
 </template>
+
 
 <script>
 import { defineComponent } from 'vue'
-import { ElButton, ElAffix, ElTabs, ElTabPane, ElImage, ElDivider, ElCol, ElRow, ElCard} from 'element-plus'
+import { ElContainer,ElMain,ElAside,ElHeader,ElCol, ElRow } from 'element-plus'
+import { ElCard,ElButton } from 'element-plus'
+import { ElAvatar} from 'element-plus'
 import '/node_modules/element-plus/dist/index.css'
 
 export default defineComponent({
   name: "ElementPlus",
   components: {
-    ElButton,
-    ElAffix,
-    ElTabs,
-    ElTabPane,
-    ElImage,
-    ElDivider,
-    ElCol,ElRow,ElCard
+    ElContainer,ElMain,ElAside,ElHeader,ElCol, ElRow,
+    ElAvatar,ElCard,ElButton,
   },
   data() {
     return {
-      src1:
-        "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
-      currentDate: new Date()
+       src1:
+         "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
+       currentDate: new Date(),
+       circleUrl:
+         'https://miro.medium.com/max/676/1*XEgA1TTwXa5AvAdw40GFow.png'
     };
   },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 20px;
-}
-.el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #fff;
-  }
-  .bg-purple {
-    background: #fff;
-  }
-  .bg-purple-light {
-    background: #fff;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #fff;
-  }
   /* 漢堡包 */
   .time {
     font-size: 13px;
@@ -142,5 +109,28 @@ export default defineComponent({
   }
   .el-card{
     padding: '0px' ;
+  }
+  .aside_avatar{
+    margin-top: 50px;
+    text-align: center;
+  }
+  .el-header {
+    background-color: #fff;
+    color: var(--el-text-color-primary);
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: inherit;
+    font-family: sans-serif;
+    font-size: 13px;
+  }
+  .el-container {
+    overflow: hidden;
+  }
+  .icon_social{
+    text-align: center;
+    padding: 0;
+    margin-top: 400px;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import { library } from "@fortawesome/fontawesome-svg-core";
+//import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faFacebookF,faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
-createApp(App).mount('#app')
+library.add(faFacebookF, fab, faInstagram);
+
+createApp(App)
+.component("font-awesome-icon", FontAwesomeIcon)
+.mount('#app')
 createApp(ElementPlus).mount('ElementPlus')
